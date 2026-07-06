c20a80f45b9f6ecc4816b427147a693facdea0fc
Abdullah Amjad <shkabdullah268@gmail.com>
2026-07-06 07:04:34 -0700

    writing the specific method impl for structs covering static behaviour and changed it using unsafe wrapper

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -3,6 +3,18 @@ struct User {
     male: bool,
     age: i32,
 }
+static mut AUTH: bool = false;
+impl User {
+    fn authenticate(&self) -> bool {
+        unsafe {
+            AUTH = true;
+            return AUTH;
+        }
+    }
+    fn is_authenticated() -> bool {
+        unsafe { AUTH }
+    }
+}
 fn main() {
     let user = User {
         name: String::from("Abdullah"),
@@ -11,5 +23,7 @@ fn main() {
     };
     println!("{}", user.name);
     print!("{}", user.age);
-    print!("{}", user.male);
+    println!("{}", user.male);
+    // _ = user.authenticate();
+    print!("is_authenticated: {}", User::is_authenticated());
 }
