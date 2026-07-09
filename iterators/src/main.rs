64c9c33f8a21071b4d45d030723764e1a583875e
Abdullah Amjad <shkabdullah268@gmail.com>
2026-07-09 04:01:06 -0700

    Experimenting with the for loop and why we need iterators

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -5,13 +5,26 @@
 //     }
 // }
 
+// fn main() {
+//     let mut vec = vec![1, 2, 3, 4, 5];
+//     let vec = vec.iter_mut();
+//     for val in vec {
+//         *val = *val - 1;
+//         println!("{:?}", val);
+//     }
+// }
+
 fn main() {
-    let mut vec = vec![1, 2, 3, 4, 5];
-    let vec = vec.iter_mut();
+    let vec = vec![1, 2, 3, 4, 5];
     for val in vec {
-        *val = *val - 1;
-        println!("{:?}", val);
+        println!("{}", val);
+        // This below line will throws the error as the ownership moves from vec to val
+        // print!("{:?}", vec)
     }
+    // This would throws the error as well cause the ownership moves to val and as the
+    // for block ends val got deallocated as well and vec already become invalidate so we lose both at this line
+    // That's why compiler saying to pass ref to sustain vec if you want to use it in future.
+    print!("{:?}", vec) //->Error
 }
 
 // iterator borrows the values from the collection that's why we were doing derefrencing
